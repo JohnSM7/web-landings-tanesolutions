e91214084a9496f1d8ec540482341d1846182727
rankusx <john.sandoval@hotmail.es>
2026-04-19 23:05:20 +0200

    Modificacion URL Tane

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tanesolution.com',
+  site: 'https://tanesolutions.com',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
